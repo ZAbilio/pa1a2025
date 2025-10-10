d40c6e3f49e4ea8b4dc32788473b5f2071b0b1f9
Aluno.Etec <aula.etec.com>
2025-10-09 21:50:19 -0300

    first comit

---
+++ soma.py
@@ -0,0 +1,18 @@
+'''a=float(input("informe o primeiro valor: "))
+b=float(input("informe o segundo valor: "))
+soma=a+b
+print(soma)'''
+
+'''nome=input("Informe o seu nome: ")
+idade=int(input("informe a sua idade: "))
+if idade>=18:
+    print(" você",nome, "é maior de idade")
+else:
+    print("você",nome, "é menor de idade")'''
+nota=float(input("informe a nota do aluno: "))
+if nota >=7:
+    print("aprovado")
+elif nota >=5:
+    print("recuperação")
+else:
+    print("reprovado")        
