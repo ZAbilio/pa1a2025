40e70c62ec98ff472b35f2ca8a400e4e5f4ebebb
Aluno.Etec <aula.etec.com>
2025-10-13 20:34:19 -0300

    lista3

--- lista2.py
+++ lista2.py
@@ -1,6 +1,47 @@
-salarioAt=float(input("informe o salario atual:"))
+'''salarioAt=float(input("informe o salario atual:"))
 perc=int(input("percentual e reajuste:"))
 perc2=perc/100
 aumento=perc2*salarioAt
 novoS=aumento+salarioAt
-print("o salario era:",salarioAt,"\n teve um aumento de:",aumento,"\n ficando com um valor de:",novoS)
+print("o salario era:",salarioAt,"\n teve um aumento de:",aumento,"\n ficando com um valor de:",novoS)'''
+
+'''valorCarro=float(input("informe o valor do carro: "))
+imp=(valorCarro*0.45)+valorCarro
+ld=(imp*0.12)+imp
+print("\t o valor do carro era",valorCarro,"\n o valor final foi de",ld,)'''
+
+'''a=int(input("Informe o primeiro valor: "))
+b=int(input("Informe o segundo valor: "))
+c=int(input("Informe o terceiro valor: "))
+d=b+c
+if a>=d:
+    print("'a' é maior que a soma de b+c")
+else:
+    print("'a' é menor que a soma de b+c")'''
+
+'''a=int(input("Informe o primeiro valor: "))
+b=int(input("Informe o segundo valor: "))
+c=int(input("Informe o terceiro valor: "))
+if a>b and a>c :
+    print("'a' é maior")
+elif b>c and b>a: 
+     print("'b' é maior que A e C")
+elif b>c: 
+     print("'b' é maior que C")
+elif a>c: 
+     print("'a' é maior que C")
+else:
+ print("'c' é maior")'''
+
+"""a=int(input("Informe o primeiro valor: "))
+b=int(input("Informe o segundo valor: "))
+c=int(input("Informe o terceiro valor: "))
+d=int(input("Informe o quarto valor: "))
+if a%2==0 or b%2==0 or c%2==0 or d%2==0"""
+
+'''ano=int(input("informe o valor do ano atual: "))
+if ano%4==0 :
+    print("ano é bissexto!")
+else:
+    print("ano não é bissexto")'''
+
