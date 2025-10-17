79093ca682505b3e3847e7e9d308c439b575e332
Aluno.Etec <aula.etec.com>
2025-10-16 22:40:22 -0300

    nova lista- laço de repetição

---
+++ repeticao.py
@@ -0,0 +1,31 @@
+'''print("calculadora em Python")
+while  True:
+   vl1=float(input("informe o primeiro valor: "))
+   vl2=float(input("informe o segundo valor: ")) 
+   sinal=input("informe o tipo de conta que deseja realizar'+,-,x,/': ")
+   match sinal:
+      case "+":
+         resultado=vl1+vl2
+         print("seu resultado é: ",resultado) 
+      case "-":
+         resultado=vl1-vl2
+         print("seu resultado é: ",resultado)
+      case "x":
+           resultado=vl1*vl2
+           print("seu resultado é: ",resultado)
+      case "/":
+           resultado=vl1/vl2
+           print("seu resultado é: ",resultado) 
+      case _:
+           print("tipo invalido")
+i=input("digite (s/n) para um novo calculo.") 
+while False:
+
+       print("programa encerrado")'''
+''' print("\n 1-soma \n 2-subtração \n 3-multiplicação \n 4-divisão")'''
+
+
+print("calculadora de media ")
+nm1=float(input("informe o valor 1: "))
+nm2=float(input("informe o valor 2: "))
+nm3=float(input("informe o valor 3: "))
