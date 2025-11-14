f28208b8e75c2bad6ae609f879c6d3668c69e169
Aluno.Etec <aula.etec.com>
2025-11-13 22:36:32 -0300

    listas

---
+++ vetoes.py
@@ -0,0 +1,26 @@
+'''salarios=[1000,2000,3000,4000]
+funcionarios=["Ana","Amanda","MARCUS","LIRA"]
+for i, salario in enumerate(salarios):
+    funcionario=funcionarios[i]
+    print("novo salario do ",funcionario,"é",salario*1.1)'''
+
+
+'''minha_lista=[]
+contador=0
+print("digite 5 valores")
+while contador<5:
+    valor=int(input("digite um valor:"))
+    minha_lista.append(valor)
+    contadro+=1
+    print(minha_lista)
+try:
+    x=int(input("Qual dos 5 valores voce quer visualizar da lista "))
+    if 1<=x<=len(minha_lista):
+        valor=minha_lista[x-1]
+        print("o valor guardaddo no indice é {x} é: {valor}")
+    else:
+        print("erro:índice fora di intervalo válido.")
+except ValueError:
+    print("Error:Entrada inválida. Digite um número inteiro.")'''
+
+
